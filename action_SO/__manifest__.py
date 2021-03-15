d4b9875c3dbd681dfc2043f3190216667bdfbf2c
Odoo-Fitex <68330713+Odoo-Fitex@users.noreply.github.com>
2021-03-15 13:50:31 +0200

    Update __manifest__.py

--- action_SO/__manifest__.py
+++ action_SO/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "action_SO",
+    'name': "Odoo14 action_SO",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,17 +10,17 @@
         Long description of module's purpose
     """,
 
-    'author': "IBS",
-    'website': "http://www.ibs-odoo.com",
+    'author': "",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Generic Modules/Human Resources',
+    'version': '14.0.1.0.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale' , 'stock' ],
+    'depends': ['base', 'sale', 'stock'],
 
     # always loaded
     'data': [
