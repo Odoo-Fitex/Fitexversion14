fa8fd40f50f722968602ac85596895ab1c38a644
Odoo-Fitex <68330713+Odoo-Fitex@users.noreply.github.com>
2021-03-15 16:22:00 +0200

    Update __manifest__.py

--- action_SO/__manifest__.py
+++ action_SO/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
+ 
     'description': """
         Long description of module's purpose
     """,
